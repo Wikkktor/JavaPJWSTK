877ab4441a0f942440e76a571de66bfdf49637ff
Wikkktor <wkaraszewicz@gmail.com>
2022-10-17 22:13:19 +0200

    adding task2

---
+++ task2/NumberSort.java
@@ -0,0 +1,31 @@
+import java.util.Scanner; 
+
+
+public class NumberSort { 
+    public static void main(String[] args) {
+        Scanner ints = new Scanner(System.in);
+        System.out.println("Enter 3 integers");
+        int a = ints.nextInt();
+        int b = ints.nextInt();
+        int c = ints.nextInt();
+        int helper;
+        System.out.println(a + " " + b + " " + c);
+        if (a > b) {
+            helper = a;
+            a = b;
+            b = helper;
+        }
+        if (b > c) {
+            helper = b;
+            b = c;
+            c = helper;
+        }
+        if (a > b) {
+            helper = a;
+            a = b;
+            b = helper;
+        }
+        System.out.println("a " + a + " b " + b + " c " + c);
+    }   
+    
+}
